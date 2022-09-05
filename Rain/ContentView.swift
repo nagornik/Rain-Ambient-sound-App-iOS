aacbd79d75d7d08c8aa81a3aa9cb6f823326598f
Anton Nagorny <nagorny.anton@gmail.com>
2022-09-05 15:35:41 +0300

    Initial Commit

---
+++ Rain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rain
+//
+//  Created by Anton Nagornyi on 05.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rain/RainApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainApp.swift
+//  Rain
+//
+//  Created by Anton Nagornyi on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct RainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
